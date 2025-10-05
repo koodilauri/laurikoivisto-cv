d554a2c225cf99c8bd539fc4da1aa328ecbe4552
Lauri <lakoivisto@gmail.com>
2025-10-06 00:17:15 +0300

    fix tailwind apply

--- packages/site/src/routes/+page.svelte
+++ packages/site/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="test">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style lang="postcss">
+	@reference "#app.css";
+
+	.test {
+		@apply text-red-500;
+	}
+</style>
